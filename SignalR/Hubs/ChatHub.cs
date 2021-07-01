fafe88150c2d8a7d7f5c0eafe078d61973faf287
anhtuan1410 <anhtuantran1410@gmail.com>
2021-07-01 17:14:01 +0700

    Add project files.

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+
+    public class ChatHubGroup : Hub
+    {
+        public Task JoinGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageGroup(MyMesseage msg)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", msg.msg);
+            await Clients.Groups(msg.groupname).SendAsync("ReceiveMessageGroup", msg.msg);
+        }
+
+        //public Task SendMessageToCaller(string user, string message)
+        //{
+        //    return Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        //}
+
+        //public Task SendMessageToGroup(string user, string message)
+        //{
+        //    return Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+        //}
+    }
+
+    public class MyMesseage
+    {
+        public string msg { get; set; }
+        public string groupname { get; set; }
+    }
+
+
+}
